b29e8d40cacea37dc153b5eff262639803053f24
Manuel Simon <info@der-supernerd.de>
2025-02-10 12:07:07 +0100

    Create records Animal, Owner and Species

--- src/main/java/de/supernerd/Main.java
+++ src/main/java/de/supernerd/Main.java
@@ -1,5 +1,7 @@
 package de.supernerd;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Owner mm = new Owner("Michael Meyer", 45, "Franz-Müller-Straße 55");
@@ -8,13 +10,19 @@ public class Main {
 
         Species sp2 = new Species("Terrier", 421);
         Owner fjm = new Owner("Franz Josef Müller", 23, "Franz-Mustermann-Straße 47");
-        Animal dog = new Animal(66, "Hund", sp2, 15, fjm);
+        Animal giraffe = new Animal(66, "Giraffe", sp2, 15, fjm);
+
+        ArrayList<Animal> animalList = new ArrayList<>();
+        animalList.add(ape);
+        animalList.add(giraffe);
+
+        Zoo zoo = new Zoo(animalList);
 
         System.out.println(ape);
-        System.out.println(dog);
+        System.out.println(giraffe);
         System.out.println(ape.age());
         System.out.println(ape.id());
-        System.out.println(dog.id());
-        System.out.println(dog.age());
+        System.out.println(giraffe.id());
+        System.out.println(giraffe.age());
     }
 }
---
+++ src/main/java/de/supernerd/Zoo.java
@@ -0,0 +1,8 @@
+package de.supernerd;
+
+import java.util.ArrayList;
+
+public record Zoo(ArrayList animals) {
+
+    
+}
